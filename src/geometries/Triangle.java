dd9b56959e9265268da61ac2d1b004fc439831fb
betkach <92046488+betkach@users.noreply.github.com>
2022-03-20 00:00:11 +0200

    triangle test + implementation

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -1,7 +1,6 @@
 package geometries;
 
-import primitives.Point;
-
+import primitives.*;
 import java.util.List;
 
 public class Triangle extends Polygon {
@@ -13,6 +12,7 @@ public class Triangle extends Polygon {
     public List<Point> getVertices(){
         return vertices;
     }
+
     @Override
     public String toString() {
         String toReturn = "the Triangle: ";
@@ -23,5 +23,30 @@ public class Triangle extends Polygon {
         }
         toReturn += ".";
         return toReturn;
+    }
+
+    @Override
+    public List<Point> findIntersections(Ray ray) {
+
+        List<Point> intersection = this.plane.findIntersections(ray);
+        if(intersection != null) { //we check in the beginning if the ray intersect the plane
+            Vector v1 = this.vertices.get(0).subtract(ray.getP0());
+            Vector v2 = this.vertices.get(1).subtract(ray.getP0());
+            Vector v3 = this.vertices.get(2).subtract(ray.getP0());
+
+            Vector v1CrossProdV2 = v1.crossProduct(v2);
+            Vector v2CrossProdV3 = v2.crossProduct(v3);
+            Vector v3CrossProdV1 = v3.crossProduct(v1);
+
+            Vector n1 = v1CrossProdV2.normalize();
+            Vector n2 = v2CrossProdV3.normalize();
+            Vector n3 = v3CrossProdV1.normalize();
+
+            if((v1.dotProduct(n1) > 0 && v2.dotProduct(n2) > 0 && v3.dotProduct(n3) > 0) || (v1.dotProduct(n1) < 0 && v2.dotProduct(n2) < 0 && v3.dotProduct(n3) < 0)) {
+                //if we entered to hear, the ray intersect the triangle
+                return intersection;
+            }
+        }
+        return null;
     }
 }
--- unittests/geometries/TriangleTest.java
+++ unittests/geometries/TriangleTest.java
@@ -2,7 +2,7 @@ package geometries;
 
 import org.junit.jupiter.api.Test;
 import primitives.Point;
-import primitives.Vector;
+import primitives.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 /**
@@ -21,6 +21,41 @@ class TriangleTest {
         // TC01: There is a simple single test here
         Triangle pl = new Triangle(new Point(0, 0, 1), new Point(1, 0, 0), new Point(0, 1, 0));
         double sqrt3 = Math.sqrt(1d / 3);
-        assertEquals(new Vector(sqrt3, sqrt3, sqrt3), pl.getNormal(new Point(0, 1, 0)), "Bad normal to trinagle");
+        assertEquals(new Vector(sqrt3, sqrt3, sqrt3), pl.getNormal(new Point(0, 1, 0)), "Bad normal to triangle");
+    }
+
+    @Test
+    void testFindIntersections() {
+        // ============ Equivalence Partitions Tests ==============
+        /*ray always intersects the plane of the triangle, we check it, before of this function*/
+
+        // TC01: There is a simple single test, ray intersect the triangle
+        Triangle triangle1 = new Triangle(new Point(1,1,1), new Point(-1,-1,1), new Point(-2,2,0));
+        Ray simpleIntersectRay = new Ray(new Point(5,0,0), new Vector(-8,0,1));
+        assertEquals(new Point(-1,0,0.75), triangle1.findIntersections(simpleIntersectRay).get(0), "Triangle.findIntersection is not working for simple intersect ray");
+
+        // TC02: There is a simple test, ray do not intersect the triangle, opposite to triangles ribs
+        Ray oppositeToRibRay = new Ray(new Point(5,0,0), new Vector(-4,0,1));
+        assertNull(triangle1.findIntersections(oppositeToRibRay), "Triangle.findIntersections is not returns null for a ray that not intersect the triangle, but intersect on the opposite of one of the triangle ribs");
+
+        // TC03: There is a simple test, ray do not intersect the triangle, opposite to triangle vertexes
+        Ray oppositeToVertexRay = new Ray(new Point(4,2,0), new Vector(-4,-4,1));
+        assertNull(triangle1.findIntersections(oppositeToVertexRay), "Triangle.findIntersections is not returns null for a point that intersect the triangle, but intersect on the opposite of one of the triangles vertexes");
+
+        // =============== Boundary Values Tests ==================
+
+        // TC04: There is test, of a ray that intersect the rib of the triangle
+        Ray intersectRibRay = new Ray(new Point(-1,1,2), new Vector(1,-1,-1));
+        //assertEquals(new Point(0,0,1), triangle1.findIntersections(intersectRibRay), "triangle.findIntersections not working for intersection in the rib");
+        assertNull(triangle1.findIntersections(intersectRibRay), "triangle.findIntersections not returns null for intersection in the rib");
+
+        // TC05: There is test, of a ray that intersect the vertex of the triangle
+        Ray intersectVertexRay = new Ray(new Point(3,3,5), new Vector(-5,-1,-5));
+        //assertEquals(new Point(-2,2,0), triangle1.findIntersections(intersectVertexRay), "triangle.findIntersections not working for intersection in the vertex");
+        assertNull(triangle1.findIntersections(intersectVertexRay), "triangle.findIntersections not returns null for intersection in the vertex");
+
+        // TC06: There is test, of a ray that intersect the continuing of the rib of the triangle
+        Ray intersectRibsContinueRay = new Ray(new Point(0,4,4), new Vector(-5,-1,-5));
+        assertNull(triangle1.findIntersections(intersectRibsContinueRay), "triangle1.findIntersections not returns null for intersection in the continuing of the rib");//the intersection in the point (-5,3,-1)
     }
 }
