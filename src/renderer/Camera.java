c5c451120efb7a9ad147a53cd5f1949ab804d584
betkach <92046488+betkach@users.noreply.github.com>
2022-03-22 16:01:05 +0200

    created camera and Camera tests + start of integrated test: Camera & Ray.findIntersections & Ray.constructRa

---
+++ src/renderer/Camera.java
@@ -0,0 +1,126 @@
+package renderer;
+
+import primitives.Point;
+import primitives.Ray;
+import primitives.Vector;
+
+import static primitives.Util.isZero;
+
+public class Camera {
+    private Point position;
+    private Vector vTo;
+    private Vector vRight;
+    private Vector vUp;
+    private double distanceCameraToViewPlane;
+    private double width;
+    private double height;
+
+    /**
+     *
+     * @param position
+     * @param vTo
+     * @param vUp
+     */
+    public Camera(Point position, Vector vTo, Vector vUp) {
+        if(!isZero(vTo.dotProduct(vUp))) //Vto is not orthogonal to vUp
+            throw new IllegalArgumentException("Vto is not orthogonal to vUp");
+        this.vTo = vTo.normalize();
+        this.vUp = vUp.normalize();
+        this.vRight = vUp.crossProduct(vTo).normalize();
+
+        this.position = position;
+        this.vTo = vTo;
+        this.vUp = vUp;
+    }
+
+    /**
+     *
+     * @param width
+     * @param height
+     * @return this
+     */
+    public Camera setVPSize(double width, double height) {
+        this.width = width;
+        this.height = height;
+        return this;
+    }
+
+    /**
+     *
+     * @param distancet
+     * @return this
+     */
+    public Camera setVPDistance(double distancet) {
+        this.distanceCameraToViewPlane = distancet;
+        return this;
+    }
+
+    /**
+     *
+     * @return Point position
+     */
+    public Point getPosition() {
+        return position;
+    }
+
+    /**
+     *
+     * @return Vector vTo
+     */
+    public Vector getvTo() {
+        return vTo;
+    }
+
+    /**
+     *
+     * @return Vector vRight
+     */
+    public Vector getvRight() {
+        return vRight;
+    }
+
+    /**
+     *
+     * @return Vector vUp
+     */
+    public Vector getvUp() {
+        return vUp;
+    }
+
+    /**
+     *
+     * @return distance from camera to view plane
+     */
+    public double getDistanceCameraToViewPlane() {
+        return distanceCameraToViewPlane;
+    }
+
+    /**
+     *
+     * @return width
+     */
+    public double getWidth() {
+        return width;
+    }
+
+    /**
+     *
+     * @return height
+     */
+    public double getHeight() {
+        return height;
+    }
+
+    /**
+     *
+     * @param nX width of view plane
+     * @param nY height of view plane
+     * @param j col
+     * @param i row
+     * @return the constructed ray.
+     */
+    public Ray constructRay(int nX, int nY, int j, int i){
+        return null;
+    }
+
+}
---
+++ unittests/integrationtests/CameraIntegrationTests.java
@@ -0,0 +1,16 @@
+package integrationtests;
+
+import org.junit.jupiter.api.Test;
+import primitives.Ray;
+
+public class CameraIntegrationTests {
+
+    /**
+     * Integrated test method for {@link renderer.Camera#constructRay(int, int, int, int)}
+     * and {@link geometries.Sphere#findIntersections(Ray)}.
+     */
+    @Test
+    void sphere() {
+
+    }
+}
