b7a6e171f5745fadec2365dfde5064191555ae1d
adadi <adadi@yatmal.co.il>
2022-03-25 16:07:42 +0300

    imageWriterTest working

--- unittests/renderer/ImageWriterTest.java
+++ unittests/renderer/ImageWriterTest.java
@@ -10,18 +10,24 @@ import static org.junit.jupiter.api.Assertions.*;
 class ImageWriterTest {
 
     @Test
-    void writeToImage(){
-        Camera camera = new Camera(new Point(0,0,0), new Vector(0,0,-1), new Vector(0,1,0))
-                .setVPSize(800,500)
+    void writeToImage() {
+        Camera camera = new Camera(new Point(0, 0, 0), new Vector(0, 0, -1), new Vector(0, 1, 0))
+                .setVPSize(16, 10)
                 .setVPDistance(10);
-        ImageWriter imageWriter = new ImageWriter("images", 16, 10);
+        ImageWriter imageWriter = new ImageWriter("images", 800, 500);
         Color background = Color.BLACK;
-        Color grid = new Color(255,23,43);
-        for (int i = 0; i < 16; i++){
-            for (int j = 0; j < 10; j ++){
-                imageWriter.writePixel(i, j, grid);
+        Color grid = new Color(255, 23, 43);
+        double squreLength = imageWriter.getNx()/16;
+        for (int i = 0; i < imageWriter.getNy(); i++) {
+            for (int j = 0; j < imageWriter.getNx(); j++) {
+                if (j % squreLength == 0 ||
+                        i % squreLength == 0)
+                    imageWriter.writePixel(j, i, grid);
+                else
+                    imageWriter.writePixel(j, i, background);
             }
         }
+        imageWriter.writeToImage();
 
     }
 }
