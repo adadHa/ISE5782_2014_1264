2a5b36bf5532309a1b38fad5046034321c8e856f
adadi <adadi@yatmal.co.il>
2022-05-12 17:45:36 +0300

    Anti Aliasing

---
+++ unittests/renderer/AntiAliasingTest.java
@@ -0,0 +1,52 @@
+/**
+ * Unit tests for primitives.Vector class
+ * @author Harel Adadi
+ */
+
+package renderer;
+
+import geometries.Sphere;
+import geometries.Triangle;
+import lighting.AmbientLight;
+import org.junit.jupiter.api.Test;
+import primitives.Color;
+import primitives.Double3;
+import primitives.Point;
+import primitives.Vector;
+import scene.Scene;
+
+import static java.awt.Color.YELLOW;
+import static org.junit.jupiter.api.Assertions.*;
+import static primitives.Util.isZero;
+
+
+class AntiAliasingTest {
+    /**
+     * Test method for {@link Vector#add(Vector)}.
+     */
+    @Test
+    void AntiAliasing(){
+        Scene scene = new Scene("Test scene")//
+                .setAmbientLight(new AmbientLight(new Color(255, 191, 191), //
+                        new Double3(1, 1, 1))) //
+                .setBackground(new Color(75, 127, 90));
+
+        scene.geometries.add(new Sphere(new Point(0, 0, -100), 50d),
+                new Triangle(new Point(-100, 0, -100), new Point(0, 100, -100), new Point(-100, 100, -100)), // up
+                // left
+                new Triangle(new Point(-100, 0, -100), new Point(0, -100, -100), new Point(-100, -100, -100)), // down
+                // left
+                new Triangle(new Point(100, 0, -100), new Point(0, -100, -100), new Point(100, -100, -100))); // down
+        // right
+        Camera camera = new Camera(Point.ZERO, new Vector(0, 0, -1), new Vector(0, 1, 0)) //
+                .setVPDistance(100) //
+                .setVPSize(500, 500) //
+                .setImageWriter(new ImageWriter("base render test - Anti Aliasing", 1000, 1000))
+                .setRayTracer(new RayTracerBasic(scene))
+                .setAntiAliasingOn(20);
+
+        camera.renderImage();
+        camera.printGrid(100, new Color(YELLOW));
+        camera.writeToImage();
+    }
+}
